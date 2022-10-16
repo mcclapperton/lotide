29d0bfb307667d4300a913dde0a859d76931f17e
Maris <mcclapperton@gmail.com>
2022-10-16 05:47:19 -0600

    using template literals instead of pluses in string concat

--- assertEqual.js
+++ assertEqual.js
@@ -1,7 +1,7 @@
-const assertEqual = function(actual, expected) {
-  const errorMsg = "😡😡😡 " + actual + " !==  " + expected;
+const assertEqual = function (actual, expected) {
+  const errorMsg = `😡😡😡 ${actual} !== ${expected}`;
   if (actual === expected) {
-    console.log("👌👌👌 Assertion Passed: " + actual + " === " + expected);
+    console.log(`👌👌👌 Assertion Passed: ${actual} === ${expected}`);
   } else console.assert(actual === expected, errorMsg);
 };
 
