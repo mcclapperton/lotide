5f88d08561808a887e7571756577841a2f3f9037
Maris <mcclapperton@gmail.com>
2022-10-24 17:57:48 -0600

    udated eqArrayTest to be true

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,4 +1,3 @@
 const assertEqual = require("../assertEqual");
 const eqArrays = require("../eqArrays");
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3, 4]), true);
-'khgcvb'= 
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
