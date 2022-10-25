8abfd7844ffa9bfa4aa20342acb7bbfea1aa5eec
Maris <mcclapperton@gmail.com>
2022-10-24 18:14:45 -0600

    adding updated headTest with M&C

--- test/headTest.js
+++ test/headTest.js
@@ -1,3 +1,14 @@
-const assertEqual = require("../assertEqual");
+const assert = require("chai").assert;
 const head = require("../head");
-assertEqual("b", head(["b", "c", "d"]));
+
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(["5"]), "5");
+  });
+  it("returns 'hi' for ['hi', 'there', 'you']", () => {
+    assert.strictEqual(head(["hi", "there", "you"]), "hi");
+  });
+});
